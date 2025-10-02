83e8af0e5037207c239fc2c2bfcb9bd5b8f6e17b
knyoman <knyoman26@gmail.com>
2025-10-03 00:41:16 +0700

    belajar increment dan decrement

--- VariabeldanOperator.php
+++ VariabeldanOperator.php
@@ -1,8 +1,20 @@
 <?php
+
+echo "<h1>Integer Float</h1>";echo "<br>";
+
+#membuat variabel
 $a = 10;
 $b = 6;
 $c = $a / $b;
 echo $c;
 echo "<br>";
 echo "Hasilnya: " . round($c); echo "<br>"; #membulatkan angka
-echo "Hasilnya: " . round($c, 3);
+echo "Hasilnya: " . round($c, 3); echo"<br>";
+
+# increment dan decrement
+$x = 20;
+$x++; # increment menambah 1
+$x--; # decrement mengurangi 1
+echo $x;
+
+
