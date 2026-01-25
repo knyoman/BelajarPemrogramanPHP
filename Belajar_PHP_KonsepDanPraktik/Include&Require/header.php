43c182f955921d8e5d79ce9efef6efd3dc7660ca
Nyoman Kusuma <knyoman26@gmail.com>
2026-01-25 23:22:18 +0700

    belajar php

---
+++ Belajar_PHP_KonsepDanPraktik/Include&Require/header.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Selamat datang ini adalah halaman Header</h1>";
+
+?>
---
+++ Belajar_PHP_KonsepDanPraktik/Include&Require/index.php
@@ -0,0 +1,6 @@
+<?php 
+include 'header.php';
+require 'header.php';
+echo "<h1>Selamat datang ini adalah halaman utama </h1>";
+
+?>
